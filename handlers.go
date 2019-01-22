55e62ee96893c2c30a9d26be511d8ce079062ae7
Vadim <vadim.pysarenko97@gmail.com>
2019-01-22 12:41:32 +0200

    add comments to handlers

--- handlers.go
+++ handlers.go
@@ -8,6 +8,10 @@ import (
 )
 
 
+//onlyGet take function (http handler) as an argument
+//and returns function that takes http.ResponseWriter and *http.Request
+//this function will call given handler only if http method of the request is GET
+//otherwise it will respond with 405 error code
 func onlyGet(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if (r.Method != http.MethodGet) {
@@ -19,6 +23,11 @@ func onlyGet(h http.HandlerFunc) http.HandlerFunc {
 }
 
 
+//getActionsHandler is http handler that takes
+//http.ResponseWriter and *http.Request as arguments
+//it tries to parse parameters from request body
+//and passes them to getActions()
+//The result of getActions() is encoded and sent as a response
 func getActionsHandler(w http.ResponseWriter, r *http.Request) {
     bytes, err := ioutil.ReadAll(r.Body)
     if err != nil {
@@ -54,6 +63,11 @@ func getActionsHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, string(b))
 }
 
+//getTransactionHandler is http handler that takes
+//http.ResponseWriter and *http.Request as arguments
+//it tries to parse parameters from request body
+//and passes them to getTransaction()
+//The result of getTransaction() is encoded and sent as a response
 func getTransactionHandler(w http.ResponseWriter, r *http.Request) {
     bytes, err := ioutil.ReadAll(r.Body)
     if err != nil {
@@ -81,6 +95,11 @@ func getTransactionHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, string(b))
 }
 
+//getKeyAccountsHandler is http handler that takes
+//http.ResponseWriter and *http.Request as arguments
+//it tries to parse parameters from request body
+//and passes them to getKeyAccounts()
+//The result of getKeyAccounts() is encoded and sent as a response
 func getKeyAccountsHandler(w http.ResponseWriter, r *http.Request) {
     bytes, err := ioutil.ReadAll(r.Body)
     if err != nil {
@@ -108,6 +127,11 @@ func getKeyAccountsHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, string(b))
 }
 
+//getControlledAccountsHandler is http handler that takes
+//http.ResponseWriter and *http.Request as arguments
+//it tries to parse parameters from request body
+//and passes them to getControlledAccounts()
+//The result of getControlledAccounts() is encoded and sent as a response
 func getControlledAccountsHandler(w http.ResponseWriter, r *http.Request) {
     bytes, err := ioutil.ReadAll(r.Body)
     if err != nil {
